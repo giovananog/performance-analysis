1362820799321dd388ec2d69fb1c620527ec78f9
Giovana Nogueira <ongiovana2016@gmail.com>
2024-09-13 16:33:24 -0300

    feat: updated code for fisrt simulator

--- first-simulator/simulator.c
+++ first-simulator/simulator.c
@@ -4,6 +4,12 @@
 #include <time.h>
 #include <float.h>
 
+typedef struct {
+    unsigned long int num_eventos;
+    double tempo_anterior;
+    double soma_areas;
+} little;
+
 double uniforme(){
     double u = rand() / ((double) RAND_MAX + 1);
     //u == 0 --> ln(u) <-- problema
@@ -19,6 +25,12 @@ double gera_tempo(double l){
 double min(double n1, double n2){
     if (n1 < n2) return n1;
     return n2;
+}
+
+void inicia_little(little *n){
+    n->num_eventos = 0;
+    n->soma_areas = 0.0;
+    n->tempo_anterior = 0.0;
 }
 
 int main(){
@@ -47,6 +59,16 @@ int main(){
 
     double soma_ocupacao = 0.0;
 
+    /**
+     * variaveis little
+     */
+
+    little en;
+    little ew_chegadas;
+    little ew_saidas;
+    inicia_little(&en);
+    inicia_little(&ew_chegadas);
+    inicia_little(&ew_saidas);
 
     while(tempo_decorrido <= tempo_simulacao){
         tempo_decorrido = 
@@ -71,6 +93,22 @@ int main(){
             tempo_chegada =
             tempo_decorrido +
             gera_tempo(parametro_chegada);
+
+            /**
+             * little
+             */
+            en.soma_areas += (tempo_decorrido - 
+              en.tempo_anterior) * en.num_eventos;
+            en.num_eventos++;
+            en.tempo_anterior = tempo_decorrido;
+
+            ew_chegadas.soma_areas += 
+              (tempo_decorrido - 
+              ew_chegadas.tempo_anterior) * 
+              ew_chegadas.num_eventos;
+            ew_chegadas.num_eventos++;
+            ew_chegadas.tempo_anterior =
+              tempo_decorrido;
         }else{
             fila--;
             tempo_saida = DBL_MAX;
@@ -83,11 +121,47 @@ int main(){
                 soma_ocupacao += tempo_saida - 
                 tempo_decorrido;
             }
+
+            /**
+             * little
+             */
+            en.soma_areas += (tempo_decorrido - 
+              en.tempo_anterior) * en.num_eventos;
+            en.num_eventos--;
+            en.tempo_anterior = tempo_decorrido;
+
+            ew_saidas.soma_areas += 
+              (tempo_decorrido - 
+              ew_saidas.tempo_anterior) * 
+              ew_saidas.num_eventos;
+            ew_saidas.num_eventos++;
+            ew_saidas.tempo_anterior =
+              tempo_decorrido;
         }
     }
 
+    ew_chegadas.soma_areas += 
+        (tempo_decorrido - 
+        ew_chegadas.tempo_anterior) * 
+        ew_chegadas.num_eventos;
+    ew_saidas.soma_areas += 
+        (tempo_decorrido - 
+        ew_saidas.tempo_anterior) * 
+        ew_saidas.num_eventos;
+
     printf("Maior tamanho de fila alcancado: %d\n",fila_max);
-    printf("Ocupacao: %lF\n", soma_ocupacao/tempo_decorrido);
+    printf("Ocupacao: %lF\n",soma_ocupacao/tempo_decorrido);
+    double en_final = en.soma_areas/tempo_decorrido;
+    double ew_final = (ew_chegadas.soma_areas - 
+      ew_saidas.soma_areas) / ew_chegadas.num_eventos;
+    double lambda = ew_chegadas.num_eventos /
+      tempo_decorrido;
+    
+    
+    printf("E[N]: %lF\n",en_final);
+    printf("E[W]: %lF\n",ew_final);
+    printf("Erro de Little: %lF\n",
+      en_final - lambda * ew_final);
 
     return 0;
 }
